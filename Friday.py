d8846a532f56b34abe0f054ad43991c520934934
preetshah283 <shahpreet2803@gmail.com>
2023-12-15 18:20:51 +0530

    speech recognition

--- Friday.py
+++ Friday.py
@@ -9,26 +9,24 @@
 
 import pyttsx3
 from datetime import datetime
+import speech_recognition as sr
 
-
+#---------------------------------------------------------------------------ENGINE INIT
 engine = pyttsx3.init() #init the speech recognition engine
 engine.setProperty("rate",180) #set the speech rate to 180 wpm
 
 voices = engine.getProperty("voices")
 # print(voices)
-
 engine.setProperty("voice",voices[1].id)
 
-time = datetime.now().time()
-# print(time)
-
-greet = time.hour
+#---------------------------------------------------------------------------SAYING THE TEXT(USER SPOKEN OR ENTERED)
 def talk(text):
     text = text + ", What can I do for you?"
+    print(text)
     engine.say(text)
     engine.runAndWait()
-    print(text)
 
+#---------------------------------------------------------------------------GREETING BASED ON TIME
 def greetings():
     if greet >= 5 and greet < 12:
         talk("Good morning!")
@@ -38,8 +36,35 @@ def greetings():
         talk("Good Evening!")
     elif greet>=0 and greet<5:
         talk("You should go to sleep.. But")
+# greetings()
 
-greetings()
+#---------------------------------------------------------------------------SPEECH RECOGNISATION
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
 
+    # recognize speech using Google Speech Recognition
+    try:
+        text = r.recognize_google(audio)
+        print("I think you said:- " + text)
+
+    except sr.UnknownValueError:
+        print("Sorry, I can't understand what you just said. Could you speak that again?")
+        text = ""
+
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        text = ""
+
+    return text
+
+time = datetime.now().time()
+# print(time)
+greet = time.hour
+# listen()
 date = datetime.now().date().strftime("%d/%m/%Y") #to change the date format
 # print(date)
+
+# query = listen().lower()
